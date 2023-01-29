2528463ca05bb897d781f63e7603ac2973a9a264
Da7labou123 <anas.chaari22@gmail.com>
2023-01-29 21:08:51 +0100

    add calcul

--- Projet.java
+++ Projet.java
@@ -3,7 +3,9 @@ public class Projet{
 
 public static void main( String[] args){
 
-    System.out.println("hello anas");
+    System.Out.println("hello anas");
 
+// Calcul somme
+    System.Out.println(5+8);
 }
 }
