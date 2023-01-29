5a978c70f04a16085cf5e2d46c1474fd16a2ab98
oumaima <oumaimachaouali5@gmail.com>
2023-01-29 21:30:48 +0100

    produit

---
+++ gt.java
@@ -0,0 +1,10 @@
+public class produit {
+    public static void main(String[] args) {
+
+        int a=1;
+        int b=2;
+        int p;
+        s = a * b;
+        System.out.println("le produit est " * p);
+    }
+}
